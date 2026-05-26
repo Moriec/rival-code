3d09901c2774c987d785ee3d3c327ca365801510
Ra <ramilpowers@gmail.com>
2026-05-27 01:36:32 +0300

    Redis cache problems-service

--- problem-service/src/main/java/com/rivalcode/problemservice/ProblemServiceApplication.java
+++ problem-service/src/main/java/com/rivalcode/problemservice/ProblemServiceApplication.java
@@ -2,8 +2,10 @@ package com.rivalcode.problemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ProblemServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProblemServiceApplication.class, args);
